01e4e7597d20eda4d08e4acf9d4ef7ff37ba6994
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-28 15:38:24 +0700

    coloring winning bttns red

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,24 @@
-const elemenGiliranSpan = document.getElementById('span-giliran');
-const arrTombolPapan = document.querySelectorAll('.tombol-papan');
-const elemenPemenangSpan = document.getElementById('span-pemenang');
-const tombolRefresh = document.getElementById('tombol-refresh');
+const elemenGiliranSpan = document.getElementById("span-giliran");
+const arrTombolPapan = document.querySelectorAll(".tombol-papan");
+const elemenPemenangSpan = document.getElementById("span-pemenang");
+const tombolRefresh = document.getElementById("tombol-refresh");
 
 const papanPermainan = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
 ];
 
-let pemenang = '';
+let pemenang = "";
 
 const objTombolPapan = {};
 for (let baris = 0; baris <= 2; baris++) {
     for (let kolom = 0; kolom <= 2; kolom++) {
-        objTombolPapan[`at${baris}${kolom}`] = document.querySelector(`.tombol-${baris}${kolom}`);
-        objTombolPapan[`at${baris}${kolom}`].addEventListener('click', (e) => {
-            if (
-                e.target.innerText === '' &&
-                !pemenang
-            ) {
+        objTombolPapan[`at${baris}${kolom}`] = document.querySelector(
+            `.tombol-${baris}${kolom}`
+        );
+        objTombolPapan[`at${baris}${kolom}`].addEventListener("click", (e) => {
+            if (e.target.innerText === "" && !pemenang) {
                 e.target.innerText = elemenGiliranSpan.innerText;
                 isiPapanPermainan(e.target.innerText, baris, kolom);
                 cekPemenang();
@@ -35,59 +34,79 @@ function isiPapanPermainan(value, baris, kolom) {
 
 function ubahGiliran() {
     const giliran = elemenGiliranSpan.innerText;
-    elemenGiliranSpan.innerText = (giliran === 'O') ? 'X': 'O';
+    elemenGiliranSpan.innerText = giliran === "O" ? "X" : "O";
 }
 
 function cekPemenang() {
     // pengecekan baris
     for (let baris = 0; baris < 3; baris++) {
         if (
-            papanPermainan[baris][0] !== '' &&
+            papanPermainan[baris][0] !== "" &&
             papanPermainan[baris][0] === papanPermainan[baris][1] &&
             papanPermainan[baris][0] === papanPermainan[baris][2]
         ) {
             pemenang = papanPermainan[baris][0]; // mengbalikan nilai pemain yang menang (X/0)
             displayPemenang();
+
+            for (let kolom = 0; kolom < 3; kolom++) {
+                objTombolPapan[`at${baris}${kolom}`].style.color = "red";
+            }
+
             return;
         }
     }
     // pengecekan kolom
     for (let kolom = 0; kolom < 3; kolom++) {
         if (
-            papanPermainan[0][kolom] !== '' &&
+            papanPermainan[0][kolom] !== "" &&
             papanPermainan[0][kolom] === papanPermainan[1][kolom] &&
             papanPermainan[0][kolom] === papanPermainan[2][kolom]
         ) {
             pemenang = papanPermainan[0][kolom];
             displayPemenang();
+
+            for (let baris = 0; baris < 3; baris++) {
+                objTombolPapan[`at${baris}${kolom}`].style.color = "red";
+            }
+
             return;
         }
     }
     // pengecekan diagonal primer
     if (
-        papanPermainan[0][0] !== '' &&
+        papanPermainan[0][0] !== "" &&
         papanPermainan[0][0] === papanPermainan[1][1] &&
         papanPermainan[0][0] === papanPermainan[2][2]
     ) {
         pemenang = papanPermainan[0][0];
         displayPemenang();
+
+        for (let index = 0; index < 3; index++) {
+            objTombolPapan[`at${index}${index}`].style.color = "red";
+        }
+
         return;
     }
     // pengecekan diagonal sekunder
     if (
-        papanPermainan[0][2] !== '' &&
+        papanPermainan[0][2] !== "" &&
         papanPermainan[0][2] === papanPermainan[1][1] &&
         papanPermainan[0][2] === papanPermainan[2][0]
     ) {
         pemenang = papanPermainan[0][2];
         displayPemenang();
+
+        for (let index = 0; index < 3; index++) {
+            objTombolPapan[`at${index}${2 - index}`].style.color = "red";
+        }
+
         return;
     }
     // pengecekan seri
     let isSeri = true;
     for (let baris = 0; baris < 3; baris++) {
         for (let kolom = 0; kolom < 3; kolom++) {
-            if (papanPermainan[baris][kolom] === '') {
+            if (papanPermainan[baris][kolom] === "") {
                 isSeri = false;
                 break;
             }
@@ -97,7 +116,7 @@ function cekPemenang() {
         }
     }
     if (isSeri) {
-        pemenang = 'Seri';
+        pemenang = "Seri";
         // displayPemenang();
         displaySeri();
         return;
@@ -109,20 +128,20 @@ function cekPemenang() {
 function displayPemenang() {
     if (!pemenang) return;
     else {
-        document.getElementById('div-pemenang').removeAttribute('hidden');
+        document.getElementById("div-pemenang").removeAttribute("hidden");
         displaySpanPemenang();
     }
 }
 
 function displaySeri() {
-    document.getElementById('div-seri').removeAttribute('hidden');
+    document.getElementById("div-seri").removeAttribute("hidden");
 }
 
 function displaySpanPemenang() {
     elemenPemenangSpan.innerText = pemenang;
 }
 
-tombolRefresh.addEventListener('click', () => location.reload());
+tombolRefresh.addEventListener("click", () => location.reload());
 
 /*arrTombolPapan.forEach((tombol) => {
     tombol.addEventListener('click', () => {
